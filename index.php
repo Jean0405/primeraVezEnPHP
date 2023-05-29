207e5efc92023434501cbf718347034e61f9a6f9
jean0405 <jeanpierrea0405@gmail.com>
2023-05-29 17:04:18 -0500

    feat: :sparkles: creamso variables  en PHP
    Se crean las primeras variables de PHP

--- index.php
+++ index.php
@@ -2,4 +2,19 @@
 // punto 5
     echo 'hola mundo';
 
+// punto 6
+    $texto = "Mundo";
+    $mensaje = printf(" %s Hola", $texto);
+    echo $mensaje;
+
+// punto 7
+    // declarmaos una variable de númerica
+    $edad = 20;
+
+    // declaramos una variable de texto
+    $nombre = "Jean";
+
+    //declaramos una variable booleana
+    $es_valido = true;
+    // echo var_dump($nombre);
 ?>
