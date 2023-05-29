31ef5e73d72c2929216dcc8480f9f9a3b61c819b
jean0405 <jeanpierrea0405@gmail.com>
2023-05-29 17:13:43 -0500

    feat: :sparkles: Tipos de datos
    Se crearon varibles de los tipos de datos más comúnes

--- index.php
+++ index.php
@@ -17,4 +17,21 @@
     //declaramos una variable booleana
     $es_valido = true;
     // echo var_dump($nombre);
+
+// punto 8
+    //Boolean
+    $logueado = true;
+    var_dump($logueado);
+    //Enteros 
+    $numeros = 200;
+    var_dump($numero);
+    //Float
+    $float = 200.5;
+    var_dump($float);
+    //String
+    $nombre = "Juan";
+    var_dump($array);
+    //Arrays
+    $array = [];
+    var_dump($array);
 ?>
