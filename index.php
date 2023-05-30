b2f218516f275143b2716ce2b3d2a148c644c73b
Jean0405 <jeanpierrea0405@gmail.com>
2023-05-30 09:23:57 -0500

    feat: :sparkles: fin primer modulo
    Finalizado la primera parte del documento usando php para manipular variables, etc

--- index.php
+++ index.php
@@ -24,14 +24,122 @@
     var_dump($logueado);
     //Enteros 
     $numeros = 200;
-    var_dump($numero);
+    var_dump($numeros);
     //Float
     $float = 200.5;
     var_dump($float);
     //String
-    $nombre = "Juan";
-    var_dump($array);
+    $nombre = "Juanes";
+    var_dump($nombre);
     //Arrays
     $array = [];
     var_dump($array);
+
+
+//punto 9
+
+/**
+ * TODO: OPERADORES ARITMETICOS
+ * * + = Suma
+ * * - = Resta
+ * * * = Multiplicación
+ * * / = División
+ * * % = Modulo
+ * * ** = Exponenciación
+ */
+
+ /**
+  * TODO: OPERADORES DE ASIGNACIÓN
+  * * = Asignación
+  * TODO: OPERADORES DE ARRAYS
+  * * + = Union
+  * * == = Igualdad
+  * * === = Identico / Estrictamente iguales
+  * * != = Diferente
+  * * <br> = Diferente 
+  * * !== = No identicos / No estrictamente iguales
+  */
+
+  /**
+   * * && = AND
+   * * || = OR
+   * * ! = NOT
+   * * and = AND
+   * * xor = XOR
+   * * or = OR
+   */
+
+   $numero1 = 20;
+   $numero2 = 30;
+   $numero3 = 30;
+   $numero4 = "30";
+
+   echo "<br/>";
+   echo "<br/>";
+
+   var_dump($numero1 > $numero2);
+   echo "<br/>";
+   
+   var_dump($numero1 < $numero2);
+   echo "<br/>";
+   
+   var_dump($numero1 >= $numero2);
+   echo "<br/>";
+
+   var_dump($numero1 <= $numero2);
+   echo "<br/>";
+
+   var_dump($numero2 == $numero3);
+   echo "<br/>";
+
+   var_dump($numero2 == $numero4);
+   echo "<br/>";
+
+   var_dump($numero2 === $numero4);
+   echo "<br/>";
+    /**
+     * * -1 Si IZQUIERDA es menor
+     * * 0 Si AMBOS son iguales
+     * * 1 Si IZQUIERDA es mayor
+     */
+    var_dump($numero1 <=> $numero4);
+    echo "<br/>";
+
+    var_dump($numero2 <=> $numero3);
+    echo "<br/>";
+
+    var_dump($numero2 <=> $numero1);
+    echo "<br/>";
+
+
+    $nombreCliente = "Campers Campuslands";
+
+    //Conocer extensión de un string
+    echo strlen($nombreCliente);
+    var_dump($nombreCliente);
+
+    //Eliminar espacios en blanco
+    $texto = trim($nombreCliente);
+    echo strlen($texto);
+    echo "<br/>";
+
+    //Convertir a mayusculas
+    echo strtoupper($nombreCliente);
+    echo "<br/>";
+
+    //Convertir a minusculas
+    echo strtolower($nombreCliente);
+
+    $mail1 = "correo@correo.com";
+    $mail2 = "Correo@correo.com";
+
+    var_dump(strtolower($mail1) === strtolower($mail2));
+    echo str_replace('Jean','J', $nombreCliente);
+
+    //Revisar si un string existe o no
+    echo strpos($nombreCliente, 'Pedro');
+    $tipoCliente = "Premium - Empresarial";
+
+    echo "<br/>"
+    
 ?>
