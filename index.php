c28d1a50929ffa76e59b512f831b1ffbe3375e57
Jean0405 <jeanpierrea0405@gmail.com>
2023-06-27 21:07:56 -0500

    feat: :sparkles: Punto 16
    Punto 16 -> Clases abstractas

--- index.php
+++ index.php
@@ -512,39 +512,70 @@
 
 
         //-----------Herencia POO------------
-        class Transporte
+        // class Transporte
+        // {
+        //     public function __construct(protected int $ruedas, protected int $capacidad)
+        //     {
+        //     }
+        //     public function getInfo(): string
+        //     {
+        //         return "El transporte tiene " . $this->ruedas . " ruedas y una capacidad de " . $this->capacidad . " personas.";
+        //     }
+        //     public function getRuedas(): int
+        //     {
+        //         return $this->ruedas;
+        //     }
+        // }
+        // class Bicicleta extends Transporte
+        // {
+        //     public function getInfo(): string
+        //     {
+        //         return "El transporte tiene " . $this->ruedas . " ruedas y una capacidad de " . $this->capacidad . " personas Y NO GASTA GASOLINA.";
+        //     }
+        // }
+        // class Automovil extends Transporte
+        // {
+        //     public function __construct(protected int $ruedas, protected int $capacidad, protected string $transmision)
+        //     {
+        //     }
+        //     public function getTransmision()
+        //     {
+        //         return $this->transmision;
+        //     }
+        // }
+        // $mercedes = new Automovil(4, 5, "Manual");
+        // echo $mercedes->getInfo();
+        // echo $mercedes->getTransmision();
+        // $bmx = new Bicicleta(2, 1);
+        // echo $bmx->getInfo();
+
+
+        //Clases abstractas
+        abstract class Animal
         {
-            public function __construct(protected int $ruedas, protected int $capacidad)
+            abstract public function hacerSonido();
+        }
+        class Capibara extends Animal
+        {
+            public function __construct()
             {
             }
-            public function getInfo(): string
+            public function hacerSonido()
             {
-                return "El transporte tiene " . $this->ruedas . " ruedas y una capacidad de " . $this->capacidad . " personas.";
-            }
-            public function getRuedas(): int
-            {
-                return $this->ruedas;
+                echo "Glu, glu, glu.";
             }
         }
-        class Bicicleta extends Transporte
+        class Camello extends Animal
         {
-            public function getInfo(): string
+            public function __construct()
             {
-                return "El transporte tiene " . $this->ruedas . " ruedas y una capacidad de " . $this->capacidad . " personas Y NO GASTA GASOLINA.";
+            }
+            public function hacerSonido()
+            {
+                echo "every go to the disco.";
             }
         }
-        class Automovil extends Transporte
-        {
-            public function __construct(protected int $ruedas, protected int $capacidad, protected string $transmision)
-            {
-            }
-            public function getTransmision()
-            {
-                return $this->transmision;
-            }
-        }
-        $mercedes = new Automovil(4, 5, "Manual");
-        echo $mercedes->getInfo();
-        echo $mercedes->getTransmision();
-        $bmx = new Bicicleta(2, 1);
-        echo $bmx->getInfo();
+        $chompiras = new Capibara();
+        $tripaSeca = new Camello();
+        $chompiras->hacerSonido();
+        $tripaSeca->hacerSonido();
